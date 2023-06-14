714b59654ddcde49f2f4cf3cab26a01e8f6631bc
2rayyy <123655023+2rayyy@users.noreply.github.com>
2023-06-14 10:18:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,17 +11,6 @@ def download_file(url, destination, proxies=None):
 file_url = "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/114.0.5735.90/win32/chrome-win32.zip"
 destination = "chrome-win32.zip"
 
-# Specify your proxy details (optional)
-http_proxy = "proxy:8080"
-https_proxy = "proxy:8080"
-ftp_proxy = "proxy:8080"
 
-#if you are not using proxies, remove the variable called proxies
-proxies = {
-    "http": http_proxy,
-    "https": https_proxy,
-    "ftp": ftp_proxy
-}
-
-#again - if you are not using proxies, remove the variable called proxies
-download_file(file_url, destination, proxies)
+#Add proxies only if needed
+download_file(file_url, destination)
